c907acf661e6207e93168bd04280d4d7a2d7139a
elliot271828 <elliot271828@gmail.com>
2023-07-26 12:28:38 -0400

    Add project files.

---
+++ MVC_BusinessLogicLayer/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVC_DataAccessLayer;
+using DomainModels;
+
+namespace MVC_BusinessLogicLayer
+{
+    public class UserService
+    {
+        private UserDataAccess userDataAccess = new UserDataAccess();
+
+        public void AddUser(User user)
+        {
+            // Here, you can add any business rules or logic before passing to the DataAccessLayer
+
+            userDataAccess.AddUser(user);
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            // Again, any business logic or rules could go here...
+
+            return userDataAccess.GetUserByUsername(username);
+        }
+    }
+}
---
+++ MVC_DataAccessLayer/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace MVC_DataAccessLayer
+{
+    public class UserService
+    {
+        private string connectionString = "Data Source=DESKTOP-RQ3BRI1\\SQLEXPRESS;Initial Catalog=master;User ID=sa;Password=123456;Connect Timeout=30";
+
+        public void AddUser(User user)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "INSERT INTO Users(Username, Email, Password, ImagePath) VALUES (@Username, @Email, @Password, @ImagePath)";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.Add(new SqlParameter("@Username", user.Username));
+                    command.Parameters.Add(new SqlParameter("@Email", user.Email));
+                    command.Parameters.Add(new SqlParameter("@Password", user.Password));
+                    command.Parameters.Add(new SqlParameter("@ImagePath", user.ImagePath ?? DBNull.Value.ToString()));
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Users WHERE Username = @Username";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.Add(new SqlParameter("@Username", username));
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        return new User
+                        {
+                            Username = reader.GetString(reader.GetOrdinal("Username")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            Password = reader.GetString(reader.GetOrdinal("Password")),
+                            ImagePath = reader.IsDBNull(reader.GetOrdinal("ImagePath")) ? null : reader.GetString(reader.GetOrdinal("ImagePath")),
+                        };
+                    }
+
+                    return null;
+                }
+            }
+        }
+    }
+}
